6f3ce9737861258b2614fbef8213e890c53580d9
saikumarseepana <saikumarseepana436@gmail.com>
2025-05-30 11:37:18 +0530

    added update and delete logic

--- apps/admin-backend/src/controllers/Category.controller.ts
+++ apps/admin-backend/src/controllers/Category.controller.ts
@@ -39,3 +39,54 @@ export const getAllCategories = async (req: Request, res: Response) => {
         return res.status(500).json({ message: "Something went wrong while fetching categories"});
     }
 }
+
+// UPDATE
+
+export const updateCategory = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const { name, description, iconUrl } = req.body;
+
+        const category = await Category.findById(id);
+        if(!category) {
+            return res.status(404).json({ message: "Category not found" });
+        }
+
+        if(name) {
+            category.name = name;
+        }
+        if(description) {
+            category.description = description;
+        }
+        if(iconUrl) {
+            category.iconUrl = iconUrl;
+        }
+
+        await category.save();
+
+        return res.status(200).json({ message: "Category updated successfully" });
+    } catch (error) {
+        console.error("Error updating category:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+//DELETE
+
+export const deleteCategory = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        console.log("🛠️ Attempting to delete category with ID:", id);
+
+        const category = await Category.findByIdAndDelete(id);
+
+        if(!category) {
+            return res.status(404).json({ message: "Category not found" });
+        }
+
+        return res.status(200).json({ message: "Category deleted successfully" });
+    } catch (error) {
+        console.error("Error deleteing category:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+};
