480dbcf66743a59212a08b97b1ec81e72346e672
saikumarseepana <saikumarseepana436@gmail.com>
2025-05-30 11:37:52 +0530

    added update and delete logic

--- apps/admin-backend/src/controllers/Subcategory.controller.ts
+++ apps/admin-backend/src/controllers/Subcategory.controller.ts
@@ -51,3 +51,54 @@ export const getAllSubcategories = async (req: Request, res: Response) => {
         return res.status(500).json({ message: "Failed to fetch subcategories"});
     }
 };
+
+// PUT /api/subcategory/update/:id
+
+export const updateSubcategory = async (req: Request, res: Response)=> {
+    try {
+        const { id } = req.params;
+        const { name, description, category } = req.body;
+
+        const subcategory = await SubCategory.findById(id);
+
+        if(!subcategory) {
+            return res.status(404).json({ message: "Subcategory not found" });
+        }
+
+        if(name) {
+            subcategory.name = name;
+        }
+        if(description) {
+            subcategory.description = description;
+        }
+        if(category) {
+            subcategory.category = category;
+        }
+
+        await subcategory.save();
+
+        return res.status(200).json({ message: "Subcategory updated successfully" });
+
+    }catch(error) {
+        console.error("Error updating subcategory:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+// DELETE /api/subcategory/delete/:id
+
+export const deleteSubcategory = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+
+        const subcategory = await SubCategory.findByIdAndDelete(id);
+        if(!subcategory){
+            return res.status(404).json({ message: "Subcategory not found" });
+        }
+
+        return res.status(200).json({ message: "Subcategory deleted successfully" });
+    } catch (error) {
+        console.error("Error deleteing subcategory: ", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+};
