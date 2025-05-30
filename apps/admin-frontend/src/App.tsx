70d8e9e11915f7307a60876b6b1029227940e451
saikumarseepana <saikumarseepana436@gmail.com>
2025-05-30 16:14:03 +0530

    installed and updated settings for tailwind usage

---
+++ apps/admin-frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-100 dark:bg-gray-900 text-black dark:text-white">
+      <h1 className="text-3xl font-bold">Tailwind Working!</h1>
+    </div>
+  );
+}
+
+export default App;
